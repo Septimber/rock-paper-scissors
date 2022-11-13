9332228a9cfa91a987554310a5ddcb0494572bf5
Septimber <timbersept@gmail.com>
2022-11-13 19:03:18 +0800

    Created the basic framework

---
+++ script.js
@@ -0,0 +1,31 @@
+function getComputerChoice(input){
+  let result = Math.floor(Math.random() * 3)
+  if(result === 2){
+    playRound(input, 'rock')
+  }
+  else if(result === 1){
+    playRound(input, 'scissors')
+  }
+  else{
+    playRound(input, 'paper')
+  }
+}
+
+function playRound(playerSelection, computerSelection){
+  switch(playerSelection === 'rock'){
+    case computerSelection === 'scissors':
+    console.log("The player won!")
+    break
+    case computerSelection === 'paper':
+    console.log("The computer won!")
+    break
+    case computerSelection === 'rock':
+    console.log('Draw!')
+  }
+}
+
+
+function playGame(){
+  let input = prompt('Please enter rock, paper, or scissors')
+  getComputerChoice(input)
+}
